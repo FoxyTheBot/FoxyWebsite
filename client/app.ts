9cb03bab3cff4c3ac370abb60259148913b8d638
Leonardo <ambrosioleonardo57@gmail.com>
2021-12-22 17:57:28 -0300

    fixing css lol, and more

--- client/app.ts
+++ client/app.ts
@@ -1,26 +1,12 @@
-import * as express from 'express';
+import express from 'express';
 require('dotenv').config();
+const app = express();
 
-export default class App {
-    port: number;
-    constructor(port) {
-        this.port = port;
-    }
+app.use(express.static('./build/public/pages'));
+app.set('view engine', 'ejs');
 
-    startServer(): void {
-        const app = express();
+app.use('/', require("../routes/controller/updatePage"));
+app.use('/', require("../routes/controller/redirectPage"));
+app.use('/', require("../routes/auth/oauthDiscord"));
 
-        app.use(express.static('./build/public/pages'));
-        app.set('view engine', 'ejs');
-
-        app.use('/', require("../routes/controller/updatePage"));
-        app.use('/', require("../routes/controller/redirectPage"));
-        app.use('/', require("../routes/auth/oauthDiscord"));
-
-
-        app.listen(this.port, () => {
-            console.log(`[APP] Servidor iniciado na porta ${this.port}`);
-        });
-
-    }
-}
+export { app };
--- start.ts
+++ start.ts
@@ -1,3 +1,5 @@
-import { App } from './client/app';
-const client = new App(8081);
-client.startServer();
+import { app } from './client/app';
+const port = process.env.PORT || 8081;
+app.listen(port, () => {
+    console.log(`[APP] Servidor iniciado na porta ${port}`);
+});
