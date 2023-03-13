542d54fa0d63bfde2c60e5642893d8516cd353d6
WinG4merBR <wingamerbr@foxybot.win>
2023-03-13 17:06:53 -0300

    Remove `strictQuery` alert from mongoose

--- database/mongoConnect.js
+++ database/mongoConnect.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
+mongoose.set('strictQuery', true);
 
 const userSchema = new mongoose.Schema({
     _id: String,
