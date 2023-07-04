4238ec9afc9f4a0e9c5b2bfcc194f72aa6fcc700
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2023-07-04 14:17:58 -0300

    Update main page

--- public/pages/js/playAudio.js
+++ public/pages/js/playAudio.js
@@ -1,25 +1,4 @@
-function awnser(url) {
-    const audio = document.querySelector("#audio");
-    audio.pause();
-    audio.currentTime = 0;
-    ring(url);
-    hideButtons();
-}
-
-function ring(a) {
-    new Audio(a).play();
-}
-
-function stop() {
-    new Audio().pause();
-}
-
-function hideButtons() {
-    const buttons = document.querySelectorAll(".btn");
-    buttons.forEach(button => {
-        button.classList.add("button--hidden");
-    });
-    const enabled = document.querySelector(".disabled");
-    enabled.classList.remove("disabled");
-    enabled.classList.add("enabled");
+function playAudio(a) {
+    var audio = new Audio(a);
+    audio.play();
 }
