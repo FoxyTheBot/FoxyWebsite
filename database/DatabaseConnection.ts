89b9ae53e6b4dbefeaada3ca19d6d202cac04a77
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2024-11-04 20:46:31 -0300

    Update user schema

--- database/DatabaseConnection.ts
+++ database/DatabaseConnection.ts
@@ -102,7 +102,10 @@ export default class DatabaseConnection {
                 premiumKeys: [],
                 roulette: {
                     availableSpins: 5,
-                }
+                },
+                lastVote: null,
+                voteCount: 0,
+                notifiedForVote: false,
             }).save();
         }
 
